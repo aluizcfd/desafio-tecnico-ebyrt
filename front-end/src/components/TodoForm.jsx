9f909526e89c0e48db4b1d1b62b6d7a0257ba5fb
André Dantas <aluizcfd@gmail.com>
2022-02-17 10:07:57 -0300

    Input box and submit button added to TodoForm component

--- front-end/src/components/TodoForm.jsx
+++ front-end/src/components/TodoForm.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function TodoForm() {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setInput('');
+  };
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>TodoForm</h1>
-    </div>
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        onChange={handleChange}
+        name="text"
+        className="todo-input"
+      />
+      <button type="submit" className="todo-button">Add!</button>
+    </form>
   );
 }
 
