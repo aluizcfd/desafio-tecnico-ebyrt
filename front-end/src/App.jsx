8471e0424a5697969759d6eb040f5f14c51ffb51
André Dantas <aluizcfd@gmail.com>
2022-02-17 09:50:43 -0300

    Added Eslint(Airbnb), first component TodoForm, some lint corrections

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
     <div className="App">
       <h1>Todo Ebyrt</h1>
+      <TodoForm />
     </div>
   );
 }
---
+++ front-end/src/components/TodoForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TodoForm() {
+  return (
+    <div>
+      <h1>TodoForm</h1>
+    </div>
+  );
+}
+
+export default TodoForm;
